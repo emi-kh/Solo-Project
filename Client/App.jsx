ee455736b9c5e8e79bac92fda193e80ddec8e6ea
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2023-12-23 13:33:57 -0500

    adjusted splash page, working on homepage enter button

--- Client/App.jsx
+++ Client/App.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// import LandingPage from './containers/landing.jsx';
-// import LoginPage from './components/login.jsx';
+import SplashPage from './pages/splash.jsx';
 
-{/* <div>
-< LoginPage /> 
-</div> */}
 
 const App = () => {
   return (
-        <div>
-        <header>
-          <h1>NoteNest</h1>
-        </header>
-        <main>
-          <div id='loginForm'>
-            <form method='POST' action='/login'>
-              <input name="username" type="text" placeholder="username"></input>
-              <input name="password" type="password" placeholder="password"></input>
-              <input type='submit' value="login"></input>
-            </form>
-          </div>
-          <div id='signupForm'>
-            <form method='POST' action='/signup'>
-              <input name="username" type="text" placeholder="username"></input>
-              <input name="password" type="password"></input>
-              <input type="submit" value="create user"></input>
-            </form>
-          </div>
-        </main>
-      </div>
+    <div>
+      <SplashPage />
+    </div>
   )
 }
 
---
+++ Client/pages/splash.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import LoginPage from '../components/login.jsx';
+import Homepage from './pages/homepage.jsx';
+
+const SplashPage = () => {
+  
+  const handleEnter = (e) => {
+    if (e) {
+      fetch('/homepage')
+        .then(res = res.json())
+        .catch(err => console.log(`Error ${err}, found in splash.jsx line 11 fetch res to /homepage`))
+    }
+  }
+
+  return (
+    <div id='splashMain'>
+      {/* < LoginPage /> */}
+      <input type='submit' value="Enter" onChange={handleEnter}></input>
+    </div> 
+  );
+
+}
+
+export default SplashPage
