6070f1683bb2b487056eb74c2c0e593fd67bf00c
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2023-12-27 01:18:47 -0500

    added 404 page

---
+++ Client/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>404:</h1>
+      <h3>The page you are looking for cannot be found</h2>
+    </div>
+  )
+}
+
+export default ErrorPage;
